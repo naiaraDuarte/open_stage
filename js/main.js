a3f9459561dd3b271a04336ab3c07b4dfa071da6
naiaraDuarte <naiara.duarte123@gmail.com>
2020-07-19 18:14:26 -0300

    quase pronto

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
  $(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 550);
  });
+ //parallax
+ $('.parallax-window').parallax({imageSrc: 'img/ticket.jpeg'});
+ $('.parallax-window1').parallax({imageSrc: 'img/publicidade2.png'});
+ $('.parallax-window2').parallax({imageSrc: 'img/corta.jpeg'});
+ $('.parallax-window3').parallax({imageSrc: 'img/teatro (5).jpeg'});
+//perguntas frequentes
+ $('.collapse').collapse()
+
